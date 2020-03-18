329cab879845a55a518318eff612999e412bcdd1
Pardeshi ,Manoj Mohan <Manoj.Pardeshi@Capco.com>
2020-03-18 11:09:58 +0530

    Added navigation components

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -4,15 +4,55 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePage } from './home.page';
 import { InsuranceCalculatorComponent } from './insurance-calculator/insurance-calculator.component';
 import { LandingComponent } from './landing/landing.component';
+import { FinancialBudgetComponent } from './financial-budget/financial-budget.component';
+import { ChoosePlanComponent } from './choose-plan/choose-plan.component';
+import { CompareQuotesComponent } from './compare-quotes/compare-quotes.component';
+import { ContactComponent } from './contact/contact.component';
+import { HelpComponent } from './help/help.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {
-    path: '',
-    component: LandingComponent
-  },
-  {
     path: 'insurance-calculator',
     component: InsuranceCalculatorComponent
+  },
+  {
+    path: 'budget',
+    component: FinancialBudgetComponent
+  },
+  {
+    path: 'chooseplan',
+    component: ChoosePlanComponent
+  },
+  {
+    path: 'quotes',
+    component: CompareQuotesComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'help',
+    component: HelpComponent
+  },
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  {
+    path: 'signup',
+    component: SignupComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: '',
+    component: LandingComponent
   }
 ];
 
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -10,6 +10,14 @@ import { HomePage } from './home.page';
 import { InsuranceCalculatorComponent } from './insurance-calculator/insurance-calculator.component';
 import { LandingComponent } from './landing/landing.component';
 import { SharedModule } from 'src/app/sharedModules/shared.module';
+import { FinancialBudgetComponent } from './financial-budget/financial-budget.component';
+import { ChoosePlanComponent } from './choose-plan/choose-plan.component';
+import { CompareQuotesComponent } from './compare-quotes/compare-quotes.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ContactComponent } from './contact/contact.component';
+import { HelpComponent } from './help/help.component';
 
 @NgModule({
   imports: [
@@ -22,7 +30,15 @@ import { SharedModule } from 'src/app/sharedModules/shared.module';
   declarations: [
     HomePage,
     LandingComponent,
-    InsuranceCalculatorComponent
+    InsuranceCalculatorComponent,
+    FinancialBudgetComponent,
+    ChoosePlanComponent,
+    CompareQuotesComponent,
+    ContactComponent,
+    HelpComponent,
+    SigninComponent,
+    SignupComponent,
+    ProfileComponent
   ]
 })
 export class HomePageModule {}
