e2019604879057d6d22f4b99f1cff11c023100bc
Gupta <Anamika.Gupta@capco.com>
2020-03-19 17:32:34 +0530

    Step 3 Choose Plan

--- src/app/pages/home/choose-plan/choose-plan.component.ts
+++ src/app/pages/home/choose-plan/choose-plan.component.ts
@@ -1,4 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { HomeConstants,  } from './../home.constants';
+
+
+export interface TermInsComparision {
+  term: number;
+  comparison: string;
+  perm: number;
+  lifeplus: number;
+}
+
+const ELEMENT_DATA: TermInsComparision[] = [
+  {comparison: 'Indefinite coverage', term: 0 , perm: 1 , lifeplus:0},
+  {comparison: 'Cash value',  term: 0 , perm: 1 , lifeplus:1},
+  {comparison: 'Affordable',  term: 1 , perm: 0 , lifeplus:1},
+  {comparison: 'Refundable',  term: 0 , perm: 1 , lifeplus:1},
+  {comparison: 'Flexible contribution',  term: 0 , perm: 0 , lifeplus:1}
+];
+
 
 @Component({
   selector: 'app-choose-plan',
@@ -7,8 +25,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChoosePlanComponent implements OnInit {
 
-  constructor() { }
+  
+ displayedColumns:string[] = ['comparison', 'term', 'perm', 'lifeplus'];
+ dataSource = ELEMENT_DATA;
+ collapsedSec1= true;
+ collapsedSec2= true;
+ collapsedSec3= true;
 
-  ngOnInit() {}
+ PLAN_HEADER = HomeConstants.PLAN_HEADER;
+ PLAN_SUB_HEADER = HomeConstants.PLAN_SUB_HEADER;
+ TERM_INS_HEADER= HomeConstants.TERM_INS_HEADER;
+ TERM_INS__EXPANSION_HEADER= HomeConstants.TERM_INS__EXPANSION_HEADER;
+ TERM_INS__EXPANSION_BODY=HomeConstants.TERM_INS__EXPANSION_BODY;
+ BTN = HomeConstants.PLAN_BTN;
+ PERM_INS_HEADER= HomeConstants.PERM_INS_HEADER;
+ PERM_INS__EXPANSION_HEADER= HomeConstants.PERM_INS__EXPANSION_HEADER;
+ PERM_INS__EXPANSION_BODY=HomeConstants.PERM_INS__EXPANSION_BODY;
+ LIFE_PLUS_HEADER=HomeConstants.LIFE_PLUS_HEADER;
+ LIFE_PLUS_SUB_HEADER=HomeConstants.LIFE_PLUS_SUB_HEADER;
+ LIFE_PLUS_EXPANSION_BODY=HomeConstants.LIFE_PLUS_EXPANSION_BODY;
+ COMP_TBL_HEADER=HomeConstants.COMP_TBL_HEADER;
+ STEP_FOUR_HEADER=HomeConstants.STEP_FOUR_HEADER;
+  constructor() { }
+  ngOnInit() {
+
+  }
 
 }
+
+
+ 
--- src/app/pages/home/home.constants.ts
+++ src/app/pages/home/home.constants.ts
@@ -215,4 +215,26 @@ export class HomeConstants {
          },
       ]
    };
+
+ // Step 3: Choose Plan
+
+ public static PLAN_HEADER = 'Term vs. Perm ';
+ public static PLAN_SUB_HEADER = 'Let us help you learn what insurance works best for you and your family.';
+ public static TERM_INS_HEADER = 'TERM INSURANCE';
+ public static TERM_INS__EXPANSION_HEADER = 'What is term life insurance?';
+ public static TERM_INS__EXPANSION_BODY = 'Term life insurance is the most popular form of life insurance. You can buy life insurance that will last 5, 10, 15, 20, or 30 years. If you pass away before the term has expired, your beneficiaries will be paid out the full amount. If you pass away after the term has expired, your beneficiaries will not be paid any amount.Keep in mind that some term life policies have an age limit. For example, some companies will only cover you up to age 60. If you are 50, you are ineligible to purchase a term life policy for more than 10 years. This is an important consideration, given that the average life expectancy for men is 76.3 years and for women is 81.4 years in 2019.';
+ public static PLAN_BTN = {
+    READ_MORE: 'READ MORE',
+    MINIMIZE: 'MINIMIZE',
+    STEP_FOUR: ' STEP FOUR'
+ }
+ public static PERM_INS_HEADER = 'PERMANENT INSURANCE';
+ public static PERM_INS__EXPANSION_HEADER = 'What is permanent life insurance?';
+ public static PERM_INS__EXPANSION_BODY = 'Permanent life insurance has multiple options, some of which are based in the market. Essentially, permanent life insurance also functions as a savings account (cash value). Each month, you contribute a portion to the premium and a portion to the cash value. Permanent life insurance is much less popular because the cost can be 5x-15x the cost of term life insurance.';
+
+ public static LIFE_PLUS_HEADER = 'How is Life+ different?';
+ public static LIFE_PLUS_SUB_HEADER = '   Life+ insurance is a hybrid of term life and permanent life insurance. ';
+ public static LIFE_PLUS_EXPANSION_BODY = 'Life+ insurance is a hybrid of term life and permanent life insurance. insurance of Life+ provides you term life insurance, plus an option to save money that will stay with you even after your term expires. If you pass away before the term expires, the cash value will be dispersed to your beneficiaries in addition to the term life policy. If you pass away after the term expires, the cash value will be dispersed but you will not be paid any amount from the term life policy. That way, you have some financial safety for end of life expenses. Similar to permanent life insurance, you have the option to contribute to your cash value every month, and you have the flexibility to decide how much to contribute every month. That way, you are not subject to the exorbitant prices of permanent life insurance. At Life+, you will not have access to the cash value. It will only be dispersed at the time of your passing, or upon the term expiration (whichever comes first).';
+ public static COMP_TBL_HEADER = 'Insurance Coverage Comparison Tool';
+ public static STEP_FOUR_HEADER = 'Ready to figure out your budget?';
 }
