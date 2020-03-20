9e44df37c46ca22e302aec1bd20422cff3a2a189
Pardeshi ,Manoj Mohan <Manoj.Pardeshi@Capco.com>
2020-03-20 12:34:05 +0530

    user profile section added

--- src/app/pages/auth/profile/profile.component.ts
+++ src/app/pages/auth/profile/profile.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { properties, financialLists } from '../auth.constant';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
-
+  htmltext = properties;
+  userFinancialInfo: string[] = financialLists;
   constructor() { }
 
   ngOnInit() {}
+  trackByIndex(index, item) {
+    return index;
+  }
 
 }
