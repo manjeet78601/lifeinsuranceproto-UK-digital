172ad2678cb1a7c3948bf66c707002bb30109aa0
Pardeshi ,Manoj Mohan <Manoj.Pardeshi@Capco.com>
2020-03-26 14:52:14 +0530

    Profile page : added accordian question content along with tracker icon

--- src/app/pages/auth/profile/profile.component.ts
+++ src/app/pages/auth/profile/profile.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { properties, financialLists, Profile } from '../auth.constant';
 import { AuthService } from 'src/app/services/auth.service';
+import { LoaderService } from 'src/app/services/loader.service';
+import { DataAnalyticsService } from 'src/app/services/data-analytics.service';
 
 @Component({
   selector: 'app-profile',
@@ -11,15 +13,25 @@ export class ProfileComponent implements OnInit {
   htmltext = properties;
   userInfo: any;
   birthDate: Date;
-  userFinancialInfo: string[] = financialLists;
+  userFinancialInfo: any;
   constructor(private auth: AuthService) { }
 
   ngOnInit() {
     this.userInfo = this.auth.getUsername();
     this.birthDate = new Date(this.auth.bDate);
+    const unsub = this.auth.getUserFinancialDetails().subscribe((data) => {
+      this.userFinancialInfo = data;
+    }, err => {
+      console.log(err);
+    });
   }
   trackByIndex(index, item) {
     return index;
   }
-
+  trackQuesProgress(event, id: string| number) {
+    const currentItem = this.userFinancialInfo.find((item) => item.id === id);
+    currentItem.isAllQuestionsAnswered = currentItem.questions.every(data => {
+      return !!data.value;
+    });
+  }
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Profile } from '../pages/auth/auth.constant';
 import { of, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+financeURL =  '../../assets/json/profile/financial-details.json';
 userInfo: Profile = {userName: '', userBdate: ''};
 bDate: string;
-  constructor() { }
+  constructor(private http: HttpClient) { }
   setUsername(name: string) {
     this.userInfo.userName = name.substr(0, name.indexOf('@'));
     this.userInfo.userBdate = '02/21/1988';
@@ -15,5 +17,8 @@ bDate: string;
   }
   getUsername() {
     return this.userInfo;
+  }
+  getUserFinancialDetails() {
+    return this.http.get(this.financeURL);
   }
 }
