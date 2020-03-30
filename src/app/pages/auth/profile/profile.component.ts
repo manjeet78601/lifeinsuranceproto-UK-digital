45c5b1b6755382d3b1e49a97f651b3f1ba731209
Kulkarni,Aniruddha <Aniruddha.Kulkarni@capco.com>
2020-03-30 11:23:24 +0530

    Doughnut chart, legend position changes

--- src/app/pages/auth/profile/profile.component.ts
+++ src/app/pages/auth/profile/profile.component.ts
@@ -3,7 +3,6 @@ import { properties, financialLists, Profile } from '../auth.constant';
 import { AuthService } from 'src/app/services/auth.service';
 import { LoaderService } from 'src/app/services/loader.service';
 import { DataAnalyticsService } from 'src/app/services/data-analytics.service';
-import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-profile',
@@ -11,12 +10,31 @@ import { Chart } from 'chart.js';
   styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit, AfterViewInit {
-  @ViewChild('userFinancialAssets', {static: false}) userProfileChart: ElementRef;
-  doughnutChart: Chart;
   htmltext = properties;
   userInfo: any;
   birthDate: Date;
   userFinancialInfo: any;
+  doughnutChartLabels = [
+    'Home', 'Mortgage', 'Vehicle(s)', 'Valuables', 'Fixed Income'
+  ];
+  doughnutChartData = [12, 3, 4, 8, 19];
+  doughnutChartType = 'doughnut';
+  doughnutChartLegendColors = [{
+    backgroundColor: [
+      '#E03997',
+      '#FFE622',
+      '#767676',
+      '#FF6D75',
+      '#00B5AD'
+    ]
+  }];
+  donutOptions: any = {
+    legend: {
+      display: true,
+      position: 'right',
+    }
+  };
+
   constructor(private auth: AuthService) { }
 
   ngOnInit() {
@@ -24,39 +42,17 @@ export class ProfileComponent implements OnInit, AfterViewInit {
     this.birthDate = new Date(this.auth.bDate);
     const unsub = this.auth.getUserFinancialDetails().subscribe((data) => {
       this.userFinancialInfo = data;
-      this.drawUserProfile();
     }, err => {
       console.log(err);
     });
   }
-  ngAfterViewInit() {}
-  ionViewDidLoad() {}
-  drawUserProfile() {
-    this.doughnutChart = new Chart(this.userProfileChart.nativeElement, {
-      type: 'doughnut',
-      data: {
-        labels: ['Home', 'Mortgage', 'Vehicle(s)', 'Valuables', 'Fixed Income'],
-        datasets: [
-          {
-            label: '# of Votes',
-            data: [12, 3, 4, 8, 19],
-            backgroundColor: [
-              '#E03997',
-              '#FFE622',
-              '#767676',
-              '#FF6D75',
-              '#00B5AD'
-            ],
-            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#FF6384', '#36A2EB', '#FFCE56']
-          }
-        ]
-      }
-    });
-  }
+  ngAfterViewInit() { }
+  ionViewDidLoad() { }
+
   trackByIndex(index, item) {
     return index;
   }
-  trackQuesProgress(event, id: string| number) {
+  trackQuesProgress(event, id: string | number) {
     const currentItem = this.userFinancialInfo.find((item) => item.id === id);
     currentItem.isAllQuestionsAnswered = currentItem.questions.every(data => {
       return !!data.value;
