877df537d952ebf3e5c6e2afc14d9c77914514f1
Kulkarni,Aniruddha <Aniruddha.Kulkarni@capco.com>
2020-03-30 13:39:08 +0530

    dev reset to 5f769f0 || 27th march 2020

--- src/app/pages/home/compliance/compliance.component.ts
+++ src/app/pages/home/compliance/compliance.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HomeConstants, } from './../home.constants';
+import { Router } from '@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-compliance',
   templateUrl: './compliance.component.html',
@@ -8,8 +11,15 @@ import { HomeConstants, } from './../home.constants';
 export class ComplianceComponent implements OnInit {
 
   DOM_CONSTANTS = HomeConstants.COMPLIANCE;
-  constructor() { }
-
-  ngOnInit() {}
-
+  complianceForm: FormGroup;
+  constructor(private router: Router) {
+    this.complianceForm = new FormGroup(
+      {
+        name: new FormControl('', [Validators.required])
+      });
+  }
+  ngOnInit() { }
+  addCard() {
+    this.router.navigate(['/home/payment']);
+  }
 }
--- src/app/pages/home/landing/landing.component.ts
+++ src/app/pages/home/landing/landing.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
-
-
+import { HomeConstants} from './../home.constants';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss'],
 })
-export class LandingComponent {
-
+export class LandingComponent  {
+  DOM_CONSTANTS = HomeConstants.HOME_PAGE;
+  maritalStaus: any[] = [
+    { name: 'UnMarried' },
+    { name: 'Married' },
+    { name: 'Single' },
+    { name: 'Single Parent' },
+    { name: 'Divorced' },
+  ];
   constructor() { }
 
 
