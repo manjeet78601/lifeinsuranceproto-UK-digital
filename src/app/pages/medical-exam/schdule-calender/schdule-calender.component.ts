f7f931a4f1ef1ee4a705d69fe70da9551375dd06
Pardeshi ,Manoj Mohan <Manoj.Pardeshi@Capco.com>
2020-04-06 12:55:09 +0530

    navigation fixes from home page

--- src/app/pages/medical-exam/schdule-calender/schdule-calender.component.ts
+++ src/app/pages/medical-exam/schdule-calender/schdule-calender.component.ts
@@ -9,24 +9,24 @@ import { Calendar } from '@ionic-native/calendar';
   styleUrls: ['./schdule-calender.component.scss'],
 })
 export class SchduleCalenderComponent implements OnInit {
+  // rangesFooter = RangesFooter;
+
+
+  constructor( ) { }
   timings = ['9:00 - AM' , '9:30 -  AM' , '10:00 - AM' , '11:00 - AM' , '12:00 - PM' , '1:00 - PM'];
 
   selectedDate: any;
+  events: string[] = [];
   // name = 'Angular 6';
 
   onSelect(event) {
     console.log(event);
     this.selectedDate = event;
   }
-  events: string[] = [];
 
   addEvent(type: string, event: MatDatepickerInputEvent<Date>) {
     this.events.push(`${type}: ${event.value}`);
   }
-  // rangesFooter = RangesFooter;
-
-
-  constructor( ) { }
 
   ngOnInit() {}
 
