1a379bfdefa11ca050227379750fc5dc5b9d975c
Gupta <Anamika.Gupta@capco.com>
2020-04-09 12:36:45 +0530

    Appointment service call from myappointment cmpnt

--- src/app/properties/appointments.constant.ts
+++ src/app/properties/appointments.constant.ts
@@ -4,8 +4,8 @@ export class AppointmentsConstant {
     public static APPT = {
         APPT_HEADER: 'My Appointments',
         APPT_SUB_HEADER: 'At the appointment, you are required to present documentation with your prescription. ',
-        SCHD_HEADER: 'Quest Diagnostics',
-        SCH_DETAILS: 'You booked an appointment with Quest Diagnostics for January 2020 at 10 AM',
+        SCHD_HEADER: 'No Appointments',
+        SCH_DETAILS: 'You booked an appointment with ',
         DOC_SCHD_HEADER: 'Manhattan Health',
         DOC_SCHD_DETAILS: 'You booked an appointment with Doctor Zack Bargine for January 2020 at 10 AM at Manhattan Health'
     };
