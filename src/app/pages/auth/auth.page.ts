2057d2a02daea30c176eb5f999e8a9e01b5a7418
manjeet78601 <47239230+manjeet78601@users.noreply.github.com>
2020-04-22 17:23:07 +0530

    auth page

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -6,7 +6,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.page.html',
-  styleUrls: ['./auth.page.scss'],
+  styleUrls: ['./auth.page.scss']
 })
 export class AuthPage implements OnInit {
   loginForm: FormGroup;
